1579eb351ec294a054a6ecc7baf6a9113641d1a7
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-10-19 18:44:15 +0900

    메신저

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express =require('express');
+const cookieParser=require('cookie-parser');
 const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const passport = require('passport');
 
-dotenv.config(); // 제일 최상단
+//거의 항상 최상단 프로세스 파일 설정파일 이 줄 이후
+dotenv.config();
 const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
-app.set('port', process.env.PORT || 8001);
-app.set('view engine', 'html');
-nunjucks.configure('views', {
-  express: app,
-  watch: true,
+passportConfig(); //패스포트 설정
+app.set('port',process.env.PORT || 8001);
+//넌적스 설정방법
+app.set('view engine','html');
+nunjucks.configure('views',{
+    express:app,
+    watch:true,
 });
+//sequelize 싱크
+sequelize.sync({force:false}) //force true -> 테이블이 지워졋다 다시생김
+.then(()=>{
+    console.log('데이터베이스 연결성공');
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+//정적파일
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/img',express.static(path.join(__dirname,'uploads')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+
+//express 세션
 app.use(session({
   resave: false,
   saveUninitialized: false,
@@ -31,22 +51,30 @@ app.use(session({
     secure: false,
   },
 }));
+app.use (passport.initialize());
+app.use(passport.session());
 
-app.use('/', pageRouter);
+app.use('/',pageRouter); //pageRouter 연결
+app.use('/auth',authRouter) //authRouter
+app.use('/post',postRouter) //authRouter
 
-app.use((req, res, next) => {
-  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-  error.status = 404;
-  next(error);
+//모든 라우터 뒤에 나오는 404 미들웨어
+app.use((req,res,next)=>{
+    const error =new Error(`${req.method} ${req.url} 라우터가 없습니다`);
+    error.status=404;
+    next(error) //바로 에러 미들웨어로 넘어간다
+}); 
+
+//에러 미들웨어
+app.use((err,req,res,next)=>{
+    //res.locals - > 템플릿 엔진 변수
+    res.locals.message=err.message;
+    res.locals.error=process.env.NODE_ENV !== 'production' ? err : {};//에러스택 개발시에만 배포시 x
+    res.status(err.status || 500); //일반 서버 에러
+    res.render('error')//에러.html (넌적스)
 });
 
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.listen(app.get('port'),()=>{
+    console.log(app.get('port'),'빈 포트에서 대기중')
+}); //포트 대기
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기중');
-});
---
+++ models/user.js
@@ -0,0 +1,54 @@
+const Sequelize = require('sequelize');
+
+//클래스가 모델, 모델이 시퀄라이즈 모델,mysql테이블과 매칭
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) { //꼭 static 으로 만들기
+    return super.init({
+      email: {
+        type: Sequelize.STRING(40),
+        allowNull: true,
+        unique: true,
+      },
+      nick: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: true,
+      },
+      provider: {
+        type: Sequelize.STRING(10),
+        allowNull: false,
+        defaultValue: 'local',
+      },
+      snsId: {
+        type: Sequelize.STRING(30),
+        allowNull: true,
+      },
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'User',
+      tableName: 'users',
+      paranoid: true,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  }
+
+  static associate(db) {
+    db.User.hasMany(db.Post);
+    db.User.belongsToMany(db.User, {
+      foreignKey: 'followingId',
+      as: 'Followers',
+      through: 'Follow',
+    });
+    db.User.belongsToMany(db.User, {
+      foreignKey: 'followerId',
+      as: 'Followings',
+      through: 'Follow',
+    });
+  }
+};
